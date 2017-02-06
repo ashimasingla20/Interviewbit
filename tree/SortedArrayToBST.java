ad86953e01237aaea28de623365fa8480e15d71b
Ashima <ashima.singla@zomato.com>
2017-02-06 16:31:39 +0530

    indented

--- tree/SortedArrayToBST.java
+++ tree/SortedArrayToBST.java
@@ -14,28 +14,27 @@ A height balanced BST  :
 
 public class SortedArrayToBST {
 	class TreeNode {
-		 int val;
-		 TreeNode left;
-		 TreeNode right;
-		 TreeNode(int x) { val = x; }
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
 	}
 	public TreeNode sortedArrayToBST(final List<Integer> a) {
-		   int start =0;
-		   int end = a.size()-1;
-		   TreeNode root = BSThelper(a ,start,end);
-		   return root;
+		int start =0;
+		int end = a.size()-1;
+		TreeNode root = BSThelper(a ,start,end);
+		return root;
+	}
+    public TreeNode BSThelper(List<Integer> b,int start,int end){
+    	int mid = (start+end)/2;
+		if(start>end){
+			return null;
 		}
-		public TreeNode BSThelper(List<Integer> b,int start,int end){
-		    int mid = (start+end)/2;
-		    if(start>end){
-		        return null;
-		    }
-		    int num = b.get(mid);
-		    TreeNode root = new TreeNode(num);
-		    root.left = BSThelper(b,start,mid-1);
-		    root.right = BSThelper(b,mid+1,end);
-		    
-		    return root;
-		}
+		int num = b.get(mid);
+		TreeNode root = new TreeNode(num);
+		root.left = BSThelper(b,start,mid-1);
+		root.right = BSThelper(b,mid+1,end);
+		return root;
+	}
 
 }
